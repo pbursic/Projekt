ac212f25c474c718e07820b66284ded00ff3b207
mmatic90 <mauriziomatic1990@gmail.com>
2016-12-21 23:40:08 +0100

    Dodan folder sa bazom i klasu za kreiranje baze

---
+++ AppProject/AppProject/Baza/Baza.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace AppProject
+{
+	public static class Baza
+	{
+		static SQLiteConnection konekcija = new SQLiteConnection("Data Source=baza.db");
+		public static void Db()
+		{
+			konekcija.Open();
+			string kreiraj = "CREATE TABLE IF NOT EXISTS korisnici (id integer primary key autoincrement, " +
+				"ime nvarchar(20), prezime nvarchar(30), datum_rodjenja date, visina integer, tezina integer)";
+			SQLiteCommand createcmd = new SQLiteCommand(kreiraj, konekcija);
+			createcmd.ExecuteNonQuery();
+			konekcija.Close();
+		}
+	}
+}
